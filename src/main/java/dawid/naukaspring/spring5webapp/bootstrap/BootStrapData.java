25efc40dd04cc6e52e28d17a7bcece144781cf0c
Vartonix <szafkadawid001@gmail.com>
2022-03-30 22:47:06 +0200

    Initializing Data with Spring

---
+++ src/main/java/dawid/naukaspring/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,45 @@
+package dawid.naukaspring.spring5webapp.bootstrap;
+
+import dawid.naukaspring.spring5webapp.domain.Author;
+import dawid.naukaspring.spring5webapp.domain.Book;
+import dawid.naukaspring.spring5webapp.repositories.AuthorRepository;
+import dawid.naukaspring.spring5webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "3243636554");
+
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+
+    }
+}
