9929e126a64fecf672462e2d063cf7365e0a18b9
Vartonix <szafkadawid001@gmail.com>
2022-04-04 22:10:32 +0200

    Relationship Publisher-Book.

--- src/main/java/dawid/naukaspring/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/dawid/naukaspring/spring5webapp/bootstrap/BootStrapData.java
@@ -26,27 +26,41 @@ public class BootStrapData implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Publisher publisher = new Publisher("UKNOW Punblisher", "Kwiatowa 63", "Warsaw", "Mazowieckie", "10-010");
+
+        publisherRepository.save(publisher);
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
+
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
+
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
         publisherRepository.save(publisher);
+
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "3243636554");
 
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
 
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
+
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
         System.out.println("Number of Publisher: "+ publisherRepository.count());
+        System.out.println("Publisher number of books: "+ publisher.getBooks().size());
         System.out.println(publisher);
 
     }
--- src/main/java/dawid/naukaspring/spring5webapp/domain/Publisher.java
+++ src/main/java/dawid/naukaspring/spring5webapp/domain/Publisher.java
@@ -1,6 +1,8 @@
 package dawid.naukaspring.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -15,6 +17,11 @@ public class Publisher {
     private String state;
     private String zip;
 
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
+
+
     public Publisher() {
     }
 
@@ -24,6 +31,14 @@ public class Publisher {
         this.city = city;
         this.state = state;
         this.zip = zip;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     public Long getId() {
